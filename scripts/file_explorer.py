385032b92c0e6998254f7cb28bb75a63aef2306d
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-02-05 19:17:06 -0300

    Use MyFileExplorer to find filepath

--- scripts/file_explorer.py
+++ scripts/file_explorer.py
@@ -7,21 +7,21 @@ from ui.ui_file_explorer import Ui_file_explorer
 import webbrowser
 
 
-def cancel_file_explore():
-    app.quit()
-
-
 class MyFileExplorer(Ui_file_explorer, QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, text_path=None):
+        assert isinstance(text_path, QtWidgets.QLineEdit) or text_path is None, 'Path must be displayed on a QLineEdit'
+
         super(MyFileExplorer, self).__init__()
         self.setupUi(self)
 
         self.model = QtWidgets.QFileSystemModel()
 
+        self.text_path = text_path
+
         self.populate()
 
         self.open.clicked.connect(self.open_path)
-        self.cancel.clicked.connect(cancel_file_explore)
+        self.cancel.clicked.connect(self.cancel_file_explore)
         self.view.clicked.connect(self.view_file)
 
         self.show()
@@ -34,12 +34,18 @@ class MyFileExplorer(Ui_file_explorer, QtWidgets.QMainWindow):
         self.treeView.setSortingEnabled(True)
 
     def open_path(self):
-        app.quit()
-        print(self.model.filePath(self.treeView.currentIndex()))
+        self.close()
+        if self.text_path:
+            self.text_path.setText(self.model.filePath(self.treeView.currentIndex()))
+        else:
+            print(self.model.filePath(self.treeView.currentIndex()))
 
     def view_file(self):
         file_path = self.model.filePath(self.treeView.currentIndex())
         webbrowser.open(file_path)
+
+    def cancel_file_explore(self):
+        self.close()
 
 
 if __name__ == '__main__':
--- scripts/file_selection.py
+++ scripts/file_selection.py
@@ -1,6 +1,8 @@
 from objects.widget import MyQWidget
 from ui.ui_file_selection import Ui_file_selection
 from PySide2 import QtWidgets
+from ui.ui_file_explorer import Ui_file_explorer
+from scripts.file_explorer import MyFileExplorer
 
 
 class file_selection(MyQWidget):
@@ -13,10 +15,12 @@ class file_selection(MyQWidget):
         self.ui.find_path.clicked.connect(lambda: self.get_filepath())
 
     def get_filepath(self):
-        # Use Pyside2 File Dialog
-        dialog = QtWidgets.QFileDialog()
-        self.parent.hide()
-        name = dialog.getOpenFileName(self, "Open File", "/home", "Text Files (*.csv *.txt *.dat)")
-        self.parent.show()
-        self.ui.path.setText(name[0])
-        print(name)
+        # Use Pyside2 File Dialog - file name is weird
+        # dialog = QtWidgets.QFileDialog()
+        # self.parent.hide()
+        # name = dialog.getOpenFileName(self, "Open File", "/home", "Text Files (*.csv *.txt *.dat)")
+        # self.parent.show()
+        # self.ui.path.setText(name[0])
+
+        # Use MyFileExplorer
+        self.file_browser = MyFileExplorer(self.ui.path)
