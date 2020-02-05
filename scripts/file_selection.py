2744c8b1d07f85d9048f1299c8b2437c6c1ad78b
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-02-05 20:45:19 -0300

    Create method to validate selected file

--- scripts/file_selection.py
+++ scripts/file_selection.py
@@ -5,6 +5,16 @@ from ui.ui_file_explorer import Ui_file_explorer
 from scripts.file_explorer import MyFileExplorer
 
 
+def validate_filepath(path):
+    valid_extensions = ['csv', 'txt', 'dat']
+    if not path:
+        return False
+    if path.split('.')[-1] in valid_extensions:
+        return True
+    else:
+        return False
+
+
 class file_selection(MyQWidget):
     def __init__(self, parent):
 
@@ -15,7 +25,7 @@ class file_selection(MyQWidget):
         self.ui.find_path.clicked.connect(lambda: self.get_filepath())
 
     def get_filepath(self):
-        # Use Pyside2 File Dialog - file name is weird
+        # Use Pyside2 File Dialog - filepath come out weird
         # dialog = QtWidgets.QFileDialog()
         # self.parent.hide()
         # name = dialog.getOpenFileName(self, "Open File", "/home", "Text Files (*.csv *.txt *.dat)")
@@ -24,3 +34,12 @@ class file_selection(MyQWidget):
 
         # Use MyFileExplorer
         self.file_browser = MyFileExplorer(self)
+
+    def next(self):
+        print('path:', self.ui.path.text())
+        print(validate_filepath(self.ui.path.text()))
+        print(self.ui.time_col.text())
+        print(self.ui.input_col.text())
+        print(self.ui.output_col.text())
+        super().next()
+
