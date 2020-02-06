d97f6e869aa59e8e10dae3249a29e6d01d3da8b4
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-02-05 21:02:49 -0300

    Create method to show messagebox

--- scripts/file_selection.py
+++ scripts/file_selection.py
@@ -1,18 +1,26 @@
 from objects.widget import MyQWidget
 from ui.ui_file_selection import Ui_file_selection
-from PySide2 import QtWidgets
-from ui.ui_file_explorer import Ui_file_explorer
+
 from scripts.file_explorer import MyFileExplorer
+
+from PySide2 import QtWidgets
+
+
+def warning_message(msg_title='', msg_text=''):
+    msgbox = QtWidgets.QMessageBox()
+    msgbox.setText(msg_title)
+    msgbox.setInformativeText(msg_text)
+    msgbox.setIcon(QtWidgets.QMessageBox.Warning)
+    msgbox.exec_()
 
 
 def validate_filepath(path):
     valid_extensions = ['csv', 'txt', 'dat']
-    if not path:
-        return False
-    if path.split('.')[-1] in valid_extensions:
-        return True
+    if path and path.split('.')[-1] in valid_extensions:
+        return path
     else:
-        return False
+        warning_message('Invalide File', 'Only .txt, .csv, and .dat files are supported.')
+        return ''
 
 
 class file_selection(MyQWidget):
@@ -37,9 +45,9 @@ class file_selection(MyQWidget):
 
     def next(self):
         print('path:', self.ui.path.text())
-        print(validate_filepath(self.ui.path.text()))
+        path = validate_filepath(self.ui.path.text())
+        print('return path:', path)
         print(self.ui.time_col.text())
         print(self.ui.input_col.text())
         print(self.ui.output_col.text())
         super().next()
-
