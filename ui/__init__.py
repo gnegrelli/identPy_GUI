fb6f762e54914a884afa8c49a996fddc0e21e752
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-05-08 20:39:35 -0300

    Create package for UI files

---
+++ ui/__init__.py
@@ -0,0 +1,9 @@
+from ui.ui_initial_page import Ui_initialPage as InitialPage
+
+from ui.ui_file_explorer import Ui_file_explorer as FileExplorer
+from ui.ui_file_selection import Ui_file_selection as FileSelection
+
+from ui.ui_model_selection import Ui_model_selection as ModelSelection
+from ui.ui_method_selection import Ui_method_selection as MethodSelection
+
+from ui.ui_mvmo import Ui_mvmo_setting as MVMO
