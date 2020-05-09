4926c9ed4389756dcb93d8d4febe36b6e1eb7a64
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-05-08 23:26:52 -0300

    Improve population of method pages

--- pages/ts.py
+++ pages/ts.py
@@ -4,13 +4,11 @@ from ui import UI_TS
 
 from identpy.Method import TS
 
-from PySide2.QtWidgets import *
+from PySide2.QtWidgets import QHBoxLayout, QLineEdit, QLabel
 from PySide2.QtCore import QSize, Qt
 
 
 class ts_page(MethodWidget):
-
-
 
     def __init__(self, parent):
 
@@ -21,12 +19,6 @@ class ts_page(MethodWidget):
         super().__init__(parent)
 
         self.populate(self.parent.estimator.model.parameters.keys())
-
-    def populate(self, n):
-        print('Populating screen with %d instances' % len(n))
-        for i in n:
-            p0 = self.add_param_row(i)
-            self.initial_values.append(p0)
 
     def add_param_row(self, name):
         horizontal_layout = QHBoxLayout()
@@ -45,5 +37,6 @@ class ts_page(MethodWidget):
         return initial_value
 
     def next(self):
-        p0 = [p0.text() for p0 in self.initial_values]
+        p0 = self.entries
+        p0 = [p.text() for p in p0]
         print(p0)
