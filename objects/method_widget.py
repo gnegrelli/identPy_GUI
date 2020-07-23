e67ae1fb8b78fb8df67b340ae49b7bc4e61e452d
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-07-23 18:46:25 -0300

    Resolve ToDo: eliminate estimate method from method widgets

--- objects/method_widget.py
+++ objects/method_widget.py
@@ -12,7 +12,7 @@ class MethodWidget(BaseWidget, QWidget):
         self.populate(self.parent.estimator.model.parameters.keys())
 
         try:
-            self.ui.estimate.clicked.connect(lambda: self.estimate())
+            self.ui.estimate.clicked.connect(lambda: self.next())
         except AttributeError:
             pass
 
@@ -32,9 +32,3 @@ class MethodWidget(BaseWidget, QWidget):
     def next(self):
         if self.verify_settings():
             super().next()
-
-    def estimate(self):
-        if self.verify_settings():
-            super().next()
-            # TODO: Ideally, estimation should start when results page becomes the current page on the StackedWidget
-            self.parent.estimator()
